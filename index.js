5154b1bb63737b30c43e684e80532d91d1ccbc02
dheerajkumar.sharma <dheerajkumar.sharma@HDL-4M8TSF2.jadeglobal.com>
2018-06-19 22:03:16 +0530

    getData Rest

--- index.js
+++ index.js
@@ -2,9 +2,21 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+var user = {
+		   "user4" : {
+		      "name" : "mohit",
+		      "password" : "password4",
+		      "profession" : "teacher",
+		      "id": 4
+		   }
+		}
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/getData',function(req,res){
+	  res.end(JSON.stringify(user));
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
